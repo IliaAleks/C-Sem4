44d10924153e2eee9b838be2230b04c7fd582e23
Илья Алексеев <av98060@gmail.com>
2022-12-15 22:41:53 +0300

    Task 25

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Task 25
+double vozvst(double num, int st)
+{
+    double rezult=0.0;
+        for (int i=0; i<st; i++)
+        {
+           rezult=num*num;
+        }
+
+ return rezult;   
+}
+Console.WriteLine("Введите число, которое нужно ввести в степень: ");
+double num=double.Parse(Console.ReadLine());
+Console.WriteLine("Введите степень: ");
+int st=int.Parse(Console.ReadLine());
+Console.WriteLine($"{vozvst(num, st)}");
+
